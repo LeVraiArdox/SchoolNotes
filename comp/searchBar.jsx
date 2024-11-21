2b58f7a02e62885a431fec9ec1f914db05977f2f
Ardox <ardox@axos-project.com>
2024-11-21 09:22:59 +0100

    Changement de la police d'écriture et update sur la SB

--- comp/searchBar.jsx
+++ comp/searchBar.jsx
@@ -1,66 +1,63 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
-import { Search } from 'lucide-react';
 
 const categories = {
     'Hardware': [
-      { text: "L'ordinateur", link: "/hardware/ordinateur" },
-      { text: "Les Composants", link: "/hardware/Composants" },
-      { text: "L'Architecture et les Bus", link: "/hardware/Architecture" },
-      { text: "Système de fichiers et BIOS", link: "/hardware/BIOS" },
-      { text: "Stockage et RAID", link: "/hardware/Stockage" },
-    ],
-    'Réseaux': [
-      { text: "Comment gère-t-on un réseau ?", link: "/reseaux/gerer" },
-      { text: "De quels équipements se compose-t-il ?", link: "/reseaux/equipements" },
-      { text: "Comment ces équipements sont-ils connectés entre eux ?", link: "/reseaux/connexion" },
-      { text: "Les différentes topologies", link: "/reseaux/topologies" },
-      { text: "À quelle échelle sont-ils déployés ?", link: "/reseaux/deploy" },
-      { text: "Les modèles OSI et TCP/IP", link: "/reseaux/osi" },
-      { text: "Étude du réseau avec OSI et TCP/IP", link: "/reseaux/etudetcp" },
-      { text: "Les couches OSI", link: "/reseaux/couches" },
-      { text: "Le protocole Ethernet", link: "/reseaux/ethernet" },
-      { text: "La table CAM", link: "/reseaux/CAM" },
-      { text: "Half et Full Duplex", link: "/reseaux/duplex" },
-      { text: "Les VLAN", link: "/reseaux/vlan" },
-      { text: "ARP", link: "/reseaux/ARP" },
-      { text: "IPv4 et IPv6", link: "/reseaux/ip" },
-      { text: "Le protocole ICMP", link: "/reseaux/icmp" },
-      { text: "NAT", link: "/reseaux/NAT" },
-      { text: "Routage", link: "/reseaux/routage" },
-      { text: "Les ACL", link: "/reseaux/ACL" },
-      { text: "TCP", link: "/reseaux/TCP" },
-      { text: "UDP", link: "/reseaux/UDP" },
-      { text: "QUIC", link: "/reseaux/QUIC" },
-    ],
-    'Windows': [
-      { text: "Les bases des OS", link: "/windows/bases" },
-      { text: "Les hyperviseurs (VM)", link: "/windows/hypervisors" },
-      { text: "Les raccourcis clavier", link: "/windows/raccourcis" },
-      { text: "Glossaire", link: "/windows/glossaire" },
-      { text: "Les commandes Windows", link: "/windows/commandes" },
-      { text: "Les mises à jour", link: "/windows/upd" },
-      { text: "Installer une appli", link: "/windows/install" },
-      { text: "La base de registre", link: "/windows/regedit" },
-      { text: "Les points de restauration", link: "/windows/respoint" },
-      { text: "Les permissions NTFS", link: "/windows/ntfsperm" },
-      { text: "Les Droits", link: "/windows/droits" },
-    ],
-    'Maths': [
-      { text: "LaTeX (cours que pour moi)", link: "/maths/latex" },
-      { text: "Algorithmes", link: "/maths/algo" },
-      { text: "Les fonctions usuelles", link: "/maths/usuelles" },
-      { text: "Les fonctions carrées", link: "/maths/fonctions2" },
-      { text: "Les fonctions racine carrée", link: "/maths/fonctionV2" },
-      { text: "Les fonctions cubes", link: "/maths/fonction3" },
-      { text: "Les fonctions inverses", link: "/maths/fonctioninv" },
-    ],
-    'Git': [
-      { text: "Les bases", link: "/git/bases" },
-    ],
-    'Connaissance de l\'entreprise': [
-      { text: "Définition de l'entreprise", link: "/entreprise/definition" },
-    ],
+        { text: "L'ordinateur", link: "/hardware/ordinateur", level: 1 },
+        { text: "Les Composants", link: "/hardware/Composants", level: 1 },
+        { text: "L'Architecture et les Bus", link: "/hardware/Architecture", level: 2 },
+        { text: "Système de fichiers et BIOS", link: "/hardware/BIOS", level: 2 },
+        { text: "Stockage et RAID", link: "/hardware/Stockage", level: 3 },
+      ],
+      'Réseaux': [
+        { text: "Comment gère-t-on un réseau ?", link: "/reseaux/gerer", level: 1 },
+        { text: "De quels équipements se compose-t-il ?", link: "/reseaux/equipements", level: 1 },
+        { text: "Comment ces équipements sont-ils connectés entre eux ?", link: "/reseaux/connexion", level: 3 },
+        { text: "Les différentes topologies", link: "/reseaux/topologies", level: 2 },
+        { text: "À quelle échelle sont-ils déployés ?", link: "/reseaux/deploy", level: 2 },
+        { text: "Les modèles OSI et TCP/IP", link: "/reseaux/osi", level: 3 },
+        { text: "Étude du réseau avec OSI et TCP/IP", link: "/reseaux/etudetcp", level: 4 },
+        { text: "Les couches OSI", link: "/reseaux/couches", level: 3 },
+        { text: "Le protocole Ethernet", link: "/reseaux/ethernet", level: 2 },
+        { text: "La table CAM", link: "/reseaux/CAM", level: 2 },
+        { text: "Half et Full Duplex", link: "/reseaux/duplex", level: 2 },
+        { text: "Les VLAN", link: "/reseaux/vlan", level: 1 },
+        { text: "ARP", link: "/reseaux/ARP", level: 2 },
+        { text: "IPv4 et IPv6", link: "/reseaux/ip", level: 3 },
+        { text: "Le protocole ICMP", link: "/reseaux/icmp", level: 1 },
+        { text: "NAT", link: "/reseaux/NAT", level: 2 },
+        { text: "Routage", link: "/reseaux/routage", level: 2 },
+        { text: "Les ACL", link: "/reseaux/ACL", level: 2 },
+        { text: "TCP", link: "/reseaux/TCP", level: 2 },
+        { text: "UDP", link: "/reseaux/UDP", level: 2 },
+        { text: "QUIC", link: "/reseaux/QUIC", level: 2 },
+      ],
+      'Windows': [
+        { text: "Les bases des OS", link: "/windows/bases", level: 1 },
+        { text: "Les hyperviseurs (VM)", link: "/windows/hypervisors", level: 1 },
+        { text: "Les raccourcis clavier", link: "/windows/raccourcis", level: 1 },
+        { text: "Glossaire", link: "/windows/glossaire", level: 2 },
+        { text: "Les commandes Windows", link: "/windows/commandes", level: 3 },
+        { text: "Les mises à jour", link: "/windows/upd", level: 2 },
+        { text: "Installer une appli", link: "/windows/install", level: 1 },
+        { text: "La base de registre", link: "/windows/regedit", level: 2 },
+        { text: "Les points de restauration", link: "/windows/respoint", level: 1 },
+        { text: "Les permissions NTFS", link: "/windows/ntfsperm", level: 3 },
+        { text: "Les Droits", link: "/windows/droits", level: 2 },
+      ],
+      'Maths': [
+        { text: "Algorithmes", link: "/maths/algo", level: 3 },
+        { text: "Les fonctions usuelles", link: "/maths/usuelles", level: 3 },
+        { text: "Les fonctions carrées", link: "/maths/fonctions2", level: 1 },
+        { text: "Les fonctions racine carrée", link: "/maths/fonctionV2", level: 1 },
+        { text: "Les fonctions cubes", link: "/maths/fonction3", level: 1 },
+        { text: "Les fonctions inverses", link: "/maths/fonctioninv", level: 1 },
+      ],
+      'Git': [
+        { text: "Les bases", link: "/git/bases", level: 1 },
+      ],
+      'Connaissance de l\'entreprise': [
+        { text: "Définition de l'entreprise", link: "/entreprise/definition", level: 1 },
+      ],
   };
   
 
@@ -80,6 +77,30 @@ const SearchBar = () => {
     setResults(filteredResults);
   };
 
+  const difficulty = (level) => {
+    let t = "";
+    let color = "";
+    if (level == "1") {
+      t = "Novice";
+      color = "difficulty-novice";
+    } else if (level == "2") {
+      t = "Amateur";
+      color = "difficulty-amateur";
+    } else if (level == "3") {
+      t = "Intermédiaire";
+      color = "difficulty-intermediate";
+    } else if (level == "4") {
+      t = "Avancé";
+      color = "difficulty-advance";
+    } else if (level == "5") {
+      t = "Expert";
+      color = "difficulty-expert";
+    }
+    return (
+        <span className={`difficulty ${color}`}>{t}</span>
+    );
+}
+
   return (
     <div className="w-full max-w-md mx-auto">
       <div className="relative">
@@ -92,16 +113,14 @@ const SearchBar = () => {
         />
       </div>
       {searchTerm && results.length > 0 && (
-        <div className="mt-2 border rounded-md searchbar flex flex-col items-start ">
+        <div className="mt-2 border rounded-md searchbar flex flex-col">
           {results.map((result, index) => (
             <>
-            <Link 
-              key={index} 
-              href={result.link}
-            >
-              {result.text}
-            </Link>
-            <hr className="w-full border-neutral-200 dark:border-contrast" />
+            <div className="flex w-full justify-between group items-center relative entry">
+                <a href={result.link}>{result.text}</a>
+                {difficulty(result.level)}
+            </div>
+            <div className='seperator'></div>
             </>
           ))}
         </div>
